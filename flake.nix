d80efd6b9c0f4d9bc6e75b01a706852f1a0f5d1e
awwpotato <awwpotato@voidq.com>
2025-01-05 12:41:13 -0800

    chore: reformat flake using rfc style

--- flake.nix
+++ flake.nix
@@ -8,36 +8,42 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-    devDB,
-    systems,
-  }:
-    utils.lib.eachSystem (import systems) (system: let
-      pkgs = import nixpkgs {inherit system;};
-      db = devDB.outputs.packages.${system};
-    in {
-      devShells.default = with pkgs;
-        mkShell {
-          env = {
-            PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
-            PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
-            PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      devDB,
+      systems,
+    }:
+    utils.lib.eachSystem (import systems) (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        db = devDB.outputs.packages.${system};
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            env = {
+              PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+              PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+              PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+            };
+            buildInputs = [
+              postgresql_17
+              db.start-database
+              db.stop-database
+              db.psql-wrapped
+              bun
+              nodePackages_latest.prettier
+              openssl
+            ];
+            shellHook = ''
+              export PG_ROOT=$(git rev-parse --show-toplevel)
+            '';
           };
-          buildInputs = [
-            postgresql_17
-            db.start-database
-            db.stop-database
-            db.psql-wrapped
-            bun
-            nodePackages_latest.prettier
-            openssl
-          ];
-          shellHook = ''
-            export PG_ROOT=$(git rev-parse --show-toplevel)
-          '';
-        };
-    });
+      }
+    );
 }
