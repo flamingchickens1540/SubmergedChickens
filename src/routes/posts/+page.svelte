f08ac4b591713b9252a4c861af756ae3720ec1e8
Azalea Colburn <azaleacolburn@gmail.com>
2025-01-06 16:44:23 -0800

    feat: add potential routes

--- src/routes/posts/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import DataTable from "./(components)/data-table.svelte"
-    import { columns } from "./(components)/columns.js"
-
-    let { data } = $props()
-</script>
-
-<DataTable data={data.posts} {columns} />
