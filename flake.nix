ed4f2bb50a44bd50a46b1d860ba5ba557380a95a
awwpotato <awwpotato@voidq.com>
2025-01-08 17:07:45 -0800

    fix: remove pkgs thingy

--- flake.nix
+++ flake.nix
@@ -27,9 +27,9 @@
           with pkgs;
           mkShell {
             env = {
-              PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
-              PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
-              PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+              PRISMA_QUERY_ENGINE_LIBRARY = "${prisma-engines}/lib/libquery_engine.node";
+              PRISMA_QUERY_ENGINE_BINARY = "${prisma-engines}/bin/query-engine";
+              PRISMA_SCHEMA_ENGINE_BINARY = "${prisma-engines}/bin/schema-engine";
             };
             buildInputs = [
               postgresql_17
