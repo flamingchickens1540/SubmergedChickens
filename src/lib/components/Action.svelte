c3604a2c460c11a134348893ec86fe6e28593ead
NateLydem <146988904+belowAverageCoder5188@users.noreply.github.com>
2025-01-22 17:02:50 -0800

    feat: timeline and action components

---
+++ src/lib/components/Action.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import type { AutoActionData } from '$lib/types';
+    import { X } from 'lucide-svelte';
+
+    let {
+        action_data = $bindable(),
+        index,
+        remove
+    }: {
+        action_data: AutoActionData;
+        index: number;
+        remove: (index: number) => void;
+    } = $props();
+
+    let color = $derived(
+        action_data.success
+            ? 'bg-jungle_green/50 shadow-jungle_green/10'
+            : 'bg-flaming_red/50 shadow-flaming_red/10'
+    );
+</script>
+
+<div
+    class="group flex flex-row content-center justify-between {color} w-full rounded p-2 text-white shadow-lg"
+>
+    <span class="w-auto shrink text-clip">{action_data.action}</span>
+    <div class="flex shrink-0 flex-row content-center justify-end gap-4">
+        <button onclick={() => remove(index)}>
+            <X />
+        </button>
+    </div>
+</div>
+
---
+++ src/lib/components/Timeline.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import type { AutoActionData, AutoHeldItems, TeleHeldItems } from '$lib/types';
+	import Action from './Action.svelte';
+
+	let {
+		actions = $bindable(),
+		held = $bindable(),
+		furthest_auto_index = $bindable(),
+		displaying = $bindable()
+	}: {
+		actions: AutoActionData[];
+		held: AutoHeldItems | TeleHeldItems;
+		furthest_auto_index: number;
+		displaying: boolean;
+	} = $props();
+
+	/// Determine if currying is the right solution or if we should use a binding
+	function remove(index: number) {
+		if (furthest_auto_index >= index) furthest_auto_index--;
+		actions.splice(index, 1);
+	}
+</script>
+
+<button
+	class="fixed inset-0 transition-all will-change-transform {displaying
+		? 'backdrop-blur'
+		: 'translate-y-full'}"
+>
+	<div
+		class="no-scrollbar absolute inset-x-0 bottom-0 flex h-[50dvh] w-dvw flex-col items-center gap-3 overflow-y-scroll rounded-t-lg bg-gunmetal p-3 text-white"
+		id="timeline"
+	>
+		{#each actions as _, i}
+			<Action
+				action_data={actions[actions.length - i - 1]}
+				index={actions.length - i - 1}
+				{remove}
+			/>
+			{#if furthest_auto_index === actions.length - i - 1}
+				<hr />
+			{/if}
+		{/each}
+		{#if actions.length === 0}
+			<h3 class="m-auto font-heading font-bold">No actions yet :3</h3>
+		{/if}
+	</div>
+</button>
