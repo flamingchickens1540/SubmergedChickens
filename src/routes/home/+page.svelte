d90e63eac880c744cefc117a1e7d17c607ac297e
NateLydem <NateLydem@users.noreply.github.com>
2025-01-23 23:55:00 +0000

    style: format files

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { LogOut, Settings } from "lucide-svelte"
-    import { goto } from '$app/navigation'
+    import { goto } from "$app/navigation"
 
     let name = "<insert name>"
 </script>
@@ -12,11 +12,36 @@
         <button class="rounded p-1"><Settings /></button>
     </div>
     <div class="grid gap-2 text-xl font-semibold">
-        <button class="rounded bg-gunmetal p-2" on:click={() => {goto('pit-display')}}>Pit Display</button>
-        <button class="rounded bg-gunmetal p-2" on:click={() => {goto('qual-scout')}}>Pit Scout</button>
-        <button class="rounded bg-gunmetal p-2" on:click={() => {goto('' /* TODO: link to match scout */)}}>Match Scout</button>
-        <button class="rounded bg-gunmetal p-2" on:click={() => {goto('admin')}}>Admin</button>
-        <button class="rounded bg-gunmetal p-2" on:click={() => {goto('analysis')}}>Analysis</button>
+        <button
+            class="rounded bg-gunmetal p-2"
+            on:click={() => {
+                goto("pit-display")
+            }}>Pit Display</button
+        >
+        <button
+            class="rounded bg-gunmetal p-2"
+            on:click={() => {
+                goto("qual-scout")
+            }}>Pit Scout</button
+        >
+        <button
+            class="rounded bg-gunmetal p-2"
+            on:click={() => {
+                goto("" /* TODO: link to match scout */)
+            }}>Match Scout</button
+        >
+        <button
+            class="rounded bg-gunmetal p-2"
+            on:click={() => {
+                goto("admin")
+            }}>Admin</button
+        >
+        <button
+            class="rounded bg-gunmetal p-2"
+            on:click={() => {
+                goto("analysis")
+            }}>Analysis</button
+        >
         <button class="rounded bg-gunmetal p-2">Bug Report</button>
     </div>
 </div>
