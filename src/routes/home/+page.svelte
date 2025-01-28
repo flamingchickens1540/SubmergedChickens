85c664e6dc3e25fa12c6416e95007d5f07bebe53
Nate L. <146988904+NateLydem@users.noreply.github.com>
2025-01-28 03:56:31 +0000

    homepage (#16)
    * feat: linked homepage buttons
    
    * style: format files
    
    * feat: bug report
    
    * style: format files
    
    * fix: useless todo deleted
    
    * style: format files
    
    * fix: removed old homepage
    
    * fix: link to matchscout on homepage
    
    * fix: renamed bugreportVisible to bugReportVisible, and changed styling on bug report
    
    * style: format files
    
    ---------
    
    Co-authored-by: NateLydem <146988904+belowAverageCoder5188@users.noreply.github.com>
    Co-authored-by: NateLydem <NateLydem@users.noreply.github.com>

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-    import { LogOut, Settings } from "lucide-svelte"
+    import { FlaskRound, LogOut, Settings } from "lucide-svelte"
+    import { goto } from "$app/navigation"
 
     let name = "<insert name>"
+
+    let bugReportVisible = false
 </script>
 
 <div class="flex w-full flex-col gap-2 p-2">
@@ -11,11 +14,54 @@
         <button class="rounded p-1"><Settings /></button>
     </div>
     <div class="grid gap-2 text-xl font-semibold">
-        <button class="rounded bg-gunmetal p-2">Pit Display</button>
-        <button class="rounded bg-gunmetal p-2">Pit Scout</button>
-        <button class="rounded bg-gunmetal p-2">Match Scout</button>
-        <button class="rounded bg-gunmetal p-2">Admin</button>
-        <button class="rounded bg-gunmetal p-2">Analysis</button>
-        <button class="rounded bg-gunmetal p-2">Bug Report</button>
+        <button
+            class="rounded bg-gunmetal p-2"
+            on:click={() => {
+                goto("pit-display")
+            }}>Pit Display</button
+        >
+        <button
+            class="rounded bg-gunmetal p-2"
+            on:click={() => {
+                goto("qual-scout")
+            }}>Pit Scout</button
+        >
+        <button
+            class="rounded bg-gunmetal p-2"
+            on:click={() => {
+                goto("match-scout/prematch")
+            }}>Match Scout</button
+        >
+        <button
+            class="rounded bg-gunmetal p-2"
+            on:click={() => {
+                goto("admin")
+            }}>Admin</button
+        >
+        <button
+            class="rounded bg-gunmetal p-2"
+            on:click={() => {
+                goto("analysis")
+            }}>Analysis</button
+        >
+        <button
+            class="rounded bg-gunmetal p-2"
+            on:click={() => {
+                bugReportVisible = !bugReportVisible
+            }}>Bug Report</button
+        >
     </div>
+    <footer
+        class=" {bugReportVisible
+            ? ''
+            : 'hidden'} no-scrollbar absolute inset-x-0 bottom-0 flex max-h-[80svh] min-h-[30svh] w-dvw flex-col items-center gap-3 overflow-y-scroll rounded-t-lg bg-gunmetal p-3 text-white"
+    >
+        <textarea
+            class="min-h-36 w-full rounded bg-eerie_black p-1"
+            placeholder="bug description"
+        ></textarea>
+        <button class="mt-auto w-full rounded bg-gunmetal p-2 font-bold"
+            >Submit</button
+        >
+    </footer>
 </div>
