37341db4d7de3763281857556f279d886b91433f
Azalea Colburn <azaleacolburn@gmail.com>
2025-01-28 17:58:07 -0800

    feat: initial pairwise input screen

---
+++ src/routes/pairwise/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    const robot_one = "2910"
+    const robot_two = "1540"
+
+    const category = "defense"
+
+    let one_is_better = $state(false)
+</script>
+
+<div class="m-2 grid grid-cols-2 gap-2 text-center text-white">
+    <div class="col-span-2">
+        Please select the robot better at {category}
+    </div>
+    <button
+        class="{one_is_better ? 'bg-green-500' : 'bg-gunmetal'} rounded"
+        onclick={() => (one_is_better = true)}>{robot_one}</button
+    >
+    <button
+        class="{one_is_better ? 'bg-gunmetal' : 'bg-green-500'} rounded"
+        onclick={() => (one_is_better = false)}>{robot_two}</button
+    >
+    <button class="col-span-2 rounded bg-gunmetal">Submit</button>
+</div>
