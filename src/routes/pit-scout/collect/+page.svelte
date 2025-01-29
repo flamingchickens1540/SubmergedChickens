3d57f4b64b084fc0486ec6b01086814f74fea886
NateLydem <146988904+belowAverageCoder5188@users.noreply.github.com>
2025-01-28 17:07:39 -0800

    feat: plan for pitscouting

---
+++ src/routes/pit-scout/collect/+page.svelte
@@ -0,0 +1,22 @@
+<header
+    class="font-heading flex flex-row justify-between border-b-2 border-white/10 p-2 text-lg font-semibold"
+>
+    <!-- TODO: text that displays team number -->
+
+    <!-- TODO: text that displays current event -->
+</header>
+
+<button> Upload Image </button>
+<!-- TODO: style and code this -->
+
+<div>
+    <!-- TODO: tank, swerve, or other options for drivetrain -->
+</div>
+
+<div>
+    <!-- TODO: robot capabilities here -->
+</div>
+
+<!-- TODO: notes section here -->
+
+<!-- TODO: submit button here -->
---
+++ src/routes/pit-scout/teamlist/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Team from "./Team.svelte"
+</script>
+
+<header
+    class="font-heading flex flex-row justify-between border-b-2 border-white/10 p-2 text-lg font-semibold"
+>
+    <!-- TODO: add search bar. URL here: https://www.npmjs.com/package/svelte-search -->
+    <span>Teams</span>
+    <!-- TODO: center this -->
+
+    <!-- TODO: text that displays current event -->
+</header>
+
+<!-- TODO: display all teams at event -->
