36081252d961a359e55426da28e3784d870b0b31
NateLydem <146988904+belowAverageCoder5188@users.noreply.github.com>
2025-01-31 16:40:07 -0800

    feat: notes area and (nonfuctional) submit button

--- src/routes/pit-scout/collect/+page.svelte
+++ src/routes/pit-scout/collect/+page.svelte
@@ -28,7 +28,6 @@
     <span class="align-right">
         {event}
     </span>
-
 </header>
 <div class="flex flex-grow flex-col gap-4 p-4">
     <button class="mt-auto rounded bg-gunmetal p-2 text-lg font-semibold">
@@ -103,6 +102,11 @@
     </label>
 </div>
 
-<!-- TODO: notes section here -->
+<div class="p-2">
+    <textarea
+        class="border-red w-full flex-grow rounded bg-eerie_black p-2"
+        placeholder="bug description"
+    ></textarea>
 
-<!-- TODO: submit button here -->
+    <button class="w-full rounded bg-gunmetal p-2 font-bold">Submit</button>
+</div>
