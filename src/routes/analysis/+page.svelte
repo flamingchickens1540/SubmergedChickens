24cf9edb903fce0509f1b8ee1bd75a24298c6749
Hanya You <youh@catlin.edu>
2025-02-03 15:53:45 -0800

    feat: chart

---
+++ src/routes/analysis/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import LineChart from "@/components/charts/LineChart.svelte"
+    import BarChart from "@/components/charts/BarChart.svelte"
+</script>
+<BarChart
+    data={[
+        {
+            name: "1540",
+            values: [12, 18, 9],
+        },
+        {
+            name: "1844",
+            values: [22, 10, 15],
+        },
+    ]}
+    categories={["Auto", "Tele", "End"]}
+    title={"Total Points"}
+    xLabel={"Team Number"}
+    yLabel={"Points"}
+    width={"600px"}
+    height={"400px"}
+/>
