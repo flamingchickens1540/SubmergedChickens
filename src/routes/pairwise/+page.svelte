a38a15361e432db27d64de77385136737fb67e4d
Azalea Colburn <azaleacolburn@gmail.com>
2025-02-04 16:03:17 -0800

    feat: added heights

--- src/routes/pairwise/+page.svelte
+++ src/routes/pairwise/+page.svelte
@@ -12,12 +12,12 @@
         Please select the robot better at {category}
     </div>
     <button
-        class="{one_is_better ? 'bg-green-500' : 'bg-gunmetal'} rounded"
+        class="{one_is_better ? 'bg-green-500' : 'bg-gunmetal'} h-96 rounded"
         onclick={() => (one_is_better = true)}>{robot_one}</button
     >
     <button
         class="{one_is_better ? 'bg-gunmetal' : 'bg-green-500'} rounded"
         onclick={() => (one_is_better = false)}>{robot_two}</button
     >
-    <button class="col-span-2 rounded bg-gunmetal">Submit</button>
+    <button class="col-span-2 h-24 rounded bg-gunmetal">Submit</button>
 </div>
