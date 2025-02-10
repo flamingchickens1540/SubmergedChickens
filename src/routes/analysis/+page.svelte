9e3f05aaabb6f7f3b03f76c5bd4d7ed727c75dc6
Hanya You <youh@catlin.edu>
2025-02-10 11:06:35 -0800

    feat: team list

--- src/routes/analysis/+page.svelte
+++ src/routes/analysis/+page.svelte
@@ -1,22 +1,58 @@
-<script>
+<script lang="ts">
     import LineChart from "@/components/charts/LineChart.svelte"
     import BarChart from "@/components/charts/BarChart.svelte"
+
+    let teams: Array<{team: number, rank: string, record: string, rp: number}> = [
+		{ team: 1540, rank: "rank1", record: "record1", rp: 8 },
+        { team: 1844, rank: "rank2", record: "record2", rp: 2 }
+	]
+
+    let averagePoints: number = 30
 </script>
-<BarChart
-    data={[
-        {
-            name: "1540",
-            values: [12, 18, 9],
-        },
-        {
-            name: "1844",
-            values: [22, 10, 15],
-        },
-    ]}
-    categories={["Auto", "Tele", "End"]}
-    title={"Total Points"}
-    xLabel={"Team Number"}
-    yLabel={"Points"}
-    width={"600px"}
-    height={"400px"}
-/>
+<div
+    class="m-auto grid w-full h-96 grid-cols-2 grid-rows-4 gap-1 p-2 sm:grid-cols-3 sm:grid-rows-3 sm:gap-4 md:grid-cols-5"
+>
+    <div class="rounded border border-solid border-white p-2 col-start-1 row-span-4 text-center">
+        Teams
+        {#each teams as teams}
+            <div class="bg-gunmetal rounded border border-solid border-white p-2 mb-2">
+                Team: {teams.team} | Rank: {teams.rank} | Record: {teams.record} | RP: {teams.rp}
+            </div>
+        {/each}
+    </div>
+    <div class="rounded border border-solid border-white p-2 col-start-2 col-span-2 row-span-2">
+        <LineChart
+            data={[
+                {
+                    name: "1540",
+                    x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    y: [12, 14, 11, 13, 15, 17, 14, 16, 18, 21],
+                    color: "#fbd50b",
+                },
+                {
+                    name: "1844",
+                    x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    y: [15, 20, 14, 19, 18, 23, 21, 22, 24, 25],
+                    color: "#9440cf",
+                },
+            ]}
+            title={"Tele Points vs Match Number"}
+            xLabel={"Match Number"}
+            yLabel={"Tele Points"}
+            width={"100%"}
+            height={"100%"}
+        />
+    </div>
+    <div class="rounded border border-solid border-white p-2 col-start-2 row-start-3 row-span-2 text-center content-center">
+        Average Points
+        <p class="text-3xl">{averagePoints}</p>
+
+    </div>
+    <div class="rounded border border-solid border-white p-2 col-start-3 row-start-3 row-span-2">
+        Robot photo
+    </div>
+    <div class="rounded border border-solid border-white p-2 col-start-4 row-span-4">
+        "choose data"
+        
+    </div>
+</div>
