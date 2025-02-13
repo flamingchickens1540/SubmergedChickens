fde683e458d3dcee84554ba23bb515e526148c4c
Hanya You <youh@catlin.edu>
2025-02-13 10:00:04 -0800

    fix: layout for mobile device

--- src/routes/analysis/+page.svelte
+++ src/routes/analysis/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import LineChart from "@/components/charts/LineChart.svelte"
+    import BarChart from "@/components/charts/BarChart.svelte"
 
     let teams: Array<{team: number, rank: string, record: string, rp: number}> = [
 		{ team: 1540, rank: "rank1", record: "record1", rp: 8 },
@@ -7,29 +8,48 @@
 		{ team: 1540, rank: "rank1", record: "record1", rp: 8 },
         { team: 1844, rank: "rank2", record: "record2", rp: 2 },
 	]
+    let dataOptions: Array<string> = [
+        "PointsPerMatch",
+        "Role",
+        "CoralPerMatch",
+        "OtherChart",
+        "YetAnotherChart",
+    ]
 
     let averagePoints: number = 30
+    let selectedChart: string = "PointsPerMatch"
 </script>
 <h1 class="text-3xl text-center">Analysis</h1>
 <div
-    class="m-auto grid w-full h-screen grid-cols-2 grid-rows-4 gap-1 p-2"
+    class="m-auto grid w-full h-screen grid-cols-2 grid-rows-6 gap-1 p-2"
 >
     <div class="rounded border border-solid border-white p-2 col-start-1 col-span-1 row-span-2 text-center overflow-scroll">
         Teams
         {#each teams as teams}
-            <div
-                class="mb-2 rounded border border-solid border-white bg-gunmetal p-2"
-            >
+            <div class="mb-2 rounded border border-solid border-white bg-gunmetal p-2">
                 Team: {teams.team} | Rank: {teams.rank} | Record: {teams.record}
                 | RP: {teams.rp}
             </div>
         {/each}
     </div>
-    <div class="rounded border border-solid border-white p-2 col-start-2 col-span-1 row-span-2">
-        "choose data"
-        
+    <div class="rounded border border-solid border-white p-2 col-start-2 col-span-1 row-span-1 row-start-1 row-span-1">
+        Robor photo
     </div>
-    <div class="rounded border border-solid border-white p-2 col-start-1 col-span-2 row-span-1">
+    <div class="rounded border border-solid border-white p-2 col-start-2 col-span-1 row-start-2 row-span-2 text-left content-center overflow-scroll">
+        <ul>
+            {#each dataOptions as dataOptions}
+                <div class="mb-2 rounded border border-solid border-white bg-gunmetal p-2"><label>
+                    <input type="radio" value="{dataOptions}" bind:group={selectedChart}>
+                    {dataOptions}
+                </label></div>
+            {/each}
+        </ul>
+    </div>
+    <div class="rounded border border-solid border-white p-2 col-start-1 row-start-3 row-span-1 text-center content-center">
+        Average Points
+        <p class="text-3xl">{averagePoints}</p>
+    </div>
+    <div class="rounded border border-solid border-white p-2 col-start-1 col-span-2 row-span-3">
         <LineChart
             data={[
                 {
@@ -51,12 +71,5 @@
             width={"100%"}
             height={"100%"}
         />
-    </div>
-    <div class="rounded border border-solid border-white p-2 col-start-1 row-start-4 row-span-1 text-center content-center">
-        Average Points
-        <p class="text-3xl">{averagePoints}</p>
-    </div>
-    <div class="rounded border border-solid border-white p-2 col-start-2 col-span-1 row-start-4 row-span-1">
-        Robot photo
     </div>
 </div>
