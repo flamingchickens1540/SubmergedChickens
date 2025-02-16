2ba3ab4a8a4d8e15e9b1b946703d7024b9f2e730
Hanya You <youh@catlin.edu>
2025-02-16 13:24:49 -0800

    feat: team alliance analysis table

---
+++ src/routes/analysis/insights/+page.svelte
@@ -0,0 +1,116 @@
+<script lang="ts">
+    import LineChart from "@/components/charts/LineChart.svelte"
+    import BarChart from "@/components/charts/BarChart.svelte"
+
+    let teams: Array<{team: number, rank: string, record: string, rp: number}> = [
+		{ team: 1540, rank: "rank1", record: "record1", rp: 8 },
+        { team: 1844, rank: "rank2", record: "record2", rp: 2 },
+		{ team: 1540, rank: "rank1", record: "record1", rp: 8 },
+        { team: 1844, rank: "rank2", record: "record2", rp: 2 },
+	]
+    let dataOptions: Array<string> = [
+        "PointsPerMatch",
+        "BarChart",
+        "CoralPerMatch",
+    ]
+
+    let averagePoints: number = 30
+    let selectedChart: string = "PointsPerMatch"
+</script>
+<h1 class="text-3xl text-center">Analysis</h1>
+<div
+    class="m-auto grid w-full h-screen grid-cols-2 grid-rows-6 gap-1 p-2"
+>
+    <div class="rounded border border-solid border-white p-2 col-start-1 col-span-1 row-span-2 text-center overflow-scroll">
+        Teams
+        {#each teams as teams}
+            <div class="mb-2 rounded border border-solid border-white bg-gunmetal p-2">
+                Team: {teams.team} | Rank: {teams.rank} | Record: {teams.record}
+                | RP: {teams.rp}
+            </div>
+        {/each}
+    </div>
+    <div class="rounded border border-solid border-white p-2 col-start-2 col-span-1 row-span-1 row-start-1 row-span-1">
+        Robor photo
+    </div>
+    <div class="rounded border border-solid border-white p-2 col-start-2 col-span-1 row-start-2 row-span-2 text-left content-center overflow-scroll">
+        <ul>
+            {#each dataOptions as dataOptions}
+                <div class="mb-2 rounded border border-solid border-white bg-gunmetal p-2"><label>
+                    <input type="radio" value="{dataOptions}" bind:group={selectedChart}>
+                    {dataOptions}
+                </label></div>
+            {/each}
+        </ul>
+    </div>
+    <div class="rounded border border-solid border-white p-2 col-start-1 row-start-3 row-span-1 text-center content-center">
+        Average Points
+        <p class="text-3xl">{averagePoints}</p>
+    </div>
+    <div class="rounded border border-solid border-white p-2 col-start-1 col-span-2 row-span-3">
+        {#if selectedChart === "PointsPerMatch"}
+            <LineChart
+                data={[
+                    {
+                        name: "1540",
+                        x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                        y: [12, 14, 11, 13, 15, 17, 14, 16, 18, 21],
+                        color: "#fbd50b",
+                    },
+                    {
+                        name: "1844",
+                        x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                        y: [15, 20, 14, 19, 18, 23, 21, 22, 24, 25],
+                        color: "#9440cf",
+                    },
+                ]}
+                title={"Tele Points vs Match Number"}
+                xLabel={"Match Number"}
+                yLabel={"Tele Points"}
+                width={"100%"}
+                height={"100%"}
+            />
+        {:else if selectedChart === "BarChart"}
+            <BarChart
+                data={[
+                    {
+                        name: "1540",
+                        values: [12, 18, 9],
+                    },
+                    {
+                        name: "1844",
+                        values: [22, 10, 15],
+                    },
+                ]}
+                categories={["Auto", "Tele", "End"]}
+                title={"BarChart"}
+                xLabel={"Team Number"}
+                yLabel={"Role"}
+                width={"100%"}
+                height={"100%"}
+            />
+        {:else if selectedChart === "CoralPerMatch"}
+                <LineChart
+                data={[
+                    {
+                        name: "team3",
+                        x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                        y: [12, 14, 5, 8, 19, 10, 20, 17, 9, 21],
+                        color: "#ff0000",
+                    },
+                    {
+                        name: "team4",
+                        x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                        y: [6, 11, 14, 10, 13, 7, 5, 18, 13, 24],
+                        color: "#0000ff",
+                    },
+                ]}
+                title={"CoralPerMatch"}
+                xLabel={"Match Number"}
+                yLabel={"Coral"}
+                width={"100%"}
+                height={"100%"}
+            />
+        {/if}
+    </div>
+</div>
---
+++ src/routes/analysis/table/+page.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+
+    let { data }: { data: PageData } = $props();
+
+    type TeamAlliance = {
+        allianceNumber: string
+        teamNumber: number
+        coral: number
+        algae: number
+        ability: number
+    }
+
+    let redTeams: TeamAlliance[] = [
+        { allianceNumber: "red1", teamNumber: data.red1, coral: 11, algae: 12, ability: 13 },
+        { allianceNumber: "red2", teamNumber: data.red2, coral: 21, algae: 22, ability: 23 },
+        { allianceNumber: "red3", teamNumber: data.red3, coral: 31, algae: 32, ability: 33 },
+    ]
+    let blueTeams: TeamAlliance[] = [
+        { allianceNumber: "blue1", teamNumber: data.blue1, coral: 11, algae: 12, ability: 13 },
+        { allianceNumber: "blue2", teamNumber: data.blue2, coral: 21, algae: 22, ability: 23 },
+        { allianceNumber: "blue3", teamNumber: data.blue3, coral: 31, algae: 32, ability: 33 },
+    ]
+
+    // $effect(() => {
+    //     console.log(data.blue1) 
+
+    // })
+</script>
+
+<div class="m-auto grid grid-cols-4 gap-2 w-full h-64 p-1 border-2 border-solid border-imperial_red mb-2">
+    <div class="col-span-1 row-span-4 grid grid-rows-4 gap-2">
+        <div class="bg-imperial_red row-span-1 w-full text-center content-center">
+            Team
+        </div>
+        <div class="bg-gunmetal text-imperial_red row-span-1 w-full text-center content-center">
+            Coral
+        </div>
+        <div class="bg-gunmetal text-imperial_red row-span-1 w-full text-center content-center">
+            Algae
+        </div>
+        <div class="bg-gunmetal text-imperial_red row-span-1 w-full text-center content-center">
+            Ability
+        </div>
+    </div>
+    {#each redTeams as team}
+        <div class="col-span-1 row-span-4 grid grid-rows-4 gap-2">
+            <div class="bg-imperial_red row-span-1 w-full text-center content-center">
+                {team.allianceNumber} - {team.teamNumber}
+            </div>
+            <input class="bg-gunmetal row-span-1 w-full text-center" bind:value="{team.coral}" placeholder="{team.teamNumber} Coral" type="text">
+            <input class="bg-gunmetal row-span-1 w-full text-center" bind:value="{team.algae}" placeholder="{team.teamNumber} Algae" type="text">
+            <input class="bg-gunmetal row-span-1 w-full text-center" bind:value="{team.ability}" placeholder="{team.teamNumber} Ability" type="text">
+        </div>
+    {/each}
+</div>
+
+<div class="m-auto grid grid-cols-4 gap-2 w-full h-64 p-1 border-2 border-solid border-steel_blue">
+    <div class="col-span-1 row-span-4 grid grid-rows-4 gap-2">
+        <div class="bg-steel_blue row-span-1 w-full text-center content-center">
+            Team
+        </div>
+        <div class="bg-gunmetal text-steel_blue row-span-1 w-full text-center content-center">
+            Coral
+        </div>
+        <div class="bg-gunmetal text-steel_blue row-span-1 w-full text-center content-center">
+            Algae
+        </div>
+        <div class="bg-gunmetal text-steel_blue row-span-1 w-full text-center content-center">
+            Ability
+        </div>
+    </div>
+    {#each blueTeams as team}
+        <div class="col-span-1 row-span-4 grid grid-rows-4 gap-2">
+            <div class="bg-steel_blue row-span-1 w-full text-center content-center">
+                {team.allianceNumber} - {team.teamNumber}
+            </div>
+            <input class="bg-gunmetal row-span-1 w-full text-center" bind:value="{team.coral}" placeholder="{team.teamNumber} Coral" type="text">
+            <input class="bg-gunmetal row-span-1 w-full text-center" bind:value="{team.algae}" placeholder="{team.teamNumber} Algae" type="text">
+            <input class="bg-gunmetal row-span-1 w-full text-center" bind:value="{team.ability}" placeholder="{team.teamNumber} Ability" type="text">
+        </div>
+    {/each}
+</div>
