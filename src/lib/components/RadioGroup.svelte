19f1f5a71e42059d7ef684c6312fd605616faaa5
Jack Sturman <sturmanj@catlin.edu>
2025-02-16 16:16:24 -0800

    fix: hid radio buttons and formatting

--- src/lib/components/RadioGroup.svelte
+++ src/lib/components/RadioGroup.svelte
@@ -7,19 +7,22 @@
     let { labels, value = $bindable() }: Props = $props()
 </script>
 
-<div class="mt-0">
+<div class="mt-0 text-center">
     {#each labels as label}
         <label
-            class="m-2 ml-0 mt-auto block w-full rounded bg-gunmetal p-5 text-lg font-semibold"
+            class="m-2 ml-0 mt-auto block w-full rounded p-5 text-lg font-semibold {value ===
+            label
+                ? 'bg-xanthous'
+                : 'bg-gunmetal'}"
         >
             <input
                 type="radio"
                 name="radio-group"
                 value={label}
                 bind:group={value}
-                class:bg-xanthous={value === label}
+                class="hidden"
             />
-            {label}
+            <span>{label}</span>
         </label>
     {/each}
 </div>
