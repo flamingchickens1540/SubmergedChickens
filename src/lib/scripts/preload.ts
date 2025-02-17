c46cc1c0ca9bce2747e796dbeea17efad32ba894
Brandon-Harad <Brandon-Harad@users.noreply.github.com>
2025-02-17 07:29:56 +0000

    style: format files

--- src/lib/scripts/preload.ts
+++ src/lib/scripts/preload.ts
@@ -2,15 +2,15 @@ import { prisma } from "$lib/prisma"
 import { json } from "@sveltejs/kit"
 import { getEventKey } from "./dbUtil"
 
-export async function preload(data: {team_key: number, match_key: string}[]) {
+export async function preload(data: { team_key: number; match_key: string }[]) {
     const event_key = await getEventKey() // placeholder until the api is fully working on main
 
     if (event_key === undefined) {
         console.log("Event key not set yet!")
-        return false;
+        return false
     }
 
     await prisma.teamMatch.createMany({
-        data: data.map((match) => ({...match, event_key: event_key}))
+        data: data.map(match => ({ ...match, event_key: event_key })),
     })
 }
--- src/routes/api/loadMatches/+server.ts
+++ src/routes/api/loadMatches/+server.ts
@@ -1,9 +1,13 @@
-import { preload } from "@/scripts/preload";
-import type { RequestHandler } from "@sveltejs/kit";
-import { json } from "@sveltejs/kit";
+import { preload } from "@/scripts/preload"
+import type { RequestHandler } from "@sveltejs/kit"
+import { json } from "@sveltejs/kit"
 
 export const POST: RequestHandler = async ({ request }: any) => {
-    const {use_tba, data} : {use_tba: boolean; data: {team_key: number, match_key: string}[]} = await request.json()
+    const {
+        use_tba,
+        data,
+    }: { use_tba: boolean; data: { team_key: number; match_key: string }[] } =
+        await request.json()
     if (use_tba) {
         console.log("What's TBA?")
         return json(false)
