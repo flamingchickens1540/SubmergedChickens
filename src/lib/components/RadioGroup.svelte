eb719628feb969f5b9f0cdcd23ae7a699c7b034a
Jack Sturman <sturmanj@catlin.edu>
2025-02-16 16:11:52 -0800

    fix: use the correct elements for checkbox and radio groups

--- src/lib/components/RadioGroup.svelte
+++ src/lib/components/RadioGroup.svelte
@@ -9,12 +9,15 @@
 
 <div class="mt-0">
     {#each labels as label}
-        <button
-            onclick={() => (value = label)}
-            class="m-2 ml-0 mt-auto block w-full rounded bg-gunmetal p-5 text-lg font-semibold"
-            class:bg-xanthous={value === label}
-        >
+        <label class="m-2 ml-0 mt-auto block w-full rounded bg-gunmetal p-5 text-lg font-semibold">
+            <input
+                type="radio"
+                name="radio-group"
+                value={label}
+                bind:group={value}
+                class:bg-xanthous={value === label}
+            />
             {label}
-        </button>
+        </label>
     {/each}
 </div>
