4bf8bde62cbfdd0321facfc4f8389091d42afcd4
Brandon-Harad <Brandon-Harad@users.noreply.github.com>
2025-02-18 07:04:15 +0000

    style: format files

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,11 +5,14 @@ export const prisma = new PrismaClient().$extends({
     result: {
         teamMatch: {
             IsTBAVerified: {
-                needs: { team_key: true, match_key: true},
+                needs: { team_key: true, match_key: true },
                 async compute(teamMatch) {
-                    return await verifyTeamMatch(teamMatch.team_key, teamMatch.match_key)
+                    return await verifyTeamMatch(
+                        teamMatch.team_key,
+                        teamMatch.match_key
+                    )
                 },
-            }
-        }
-    }
+            },
+        },
+    },
 })
--- src/lib/scripts/TBAVerify.ts
+++ src/lib/scripts/TBAVerify.ts
@@ -1,15 +1,26 @@
-import { TBA_AUTH_KEY } from '$env/static/private'
+import { TBA_AUTH_KEY } from "$env/static/private"
 
-export async function verifyTeamMatch(team_key: number, match_key: string): Promise<boolean> {
-    const response = await (await fetch(`https://www.thebluealliance.com/api/v3/match/${match_key}/simple`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "X-TBA-Auth-Key": TBA_AUTH_KEY,
-        },
-    })).json()
+export async function verifyTeamMatch(
+    team_key: number,
+    match_key: string
+): Promise<boolean> {
+    const response = await (
+        await fetch(
+            `https://www.thebluealliance.com/api/v3/match/${match_key}/simple`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-TBA-Auth-Key": TBA_AUTH_KEY,
+                },
+            }
+        )
+    ).json()
 
-    const team_keys : string[] = [...response.alliances.blue.team_keys, ...response.alliances.red.team_keys].map((a) => (a.slice(3)))
+    const team_keys: string[] = [
+        ...response.alliances.blue.team_keys,
+        ...response.alliances.red.team_keys,
+    ].map(a => a.slice(3))
 
     return team_keys.includes(team_key.toString())
 }
