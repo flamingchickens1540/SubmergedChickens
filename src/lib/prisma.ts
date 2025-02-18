683a0c923727d121499ff4d9792a7d418b0c29f9
Brandon Harad <haradb@catlin.edu>
2025-02-17 23:03:56 -0800

    feat: teamMatch verify

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,15 @@
 import { PrismaClient } from "@prisma/client"
-import type { TeamMatch } from "@prisma/client"
+import { verifyTeamMatch } from "./scripts/TBAVerify"
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient().$extends({
+    result: {
+        teamMatch: {
+            IsTBAVerified: {
+                needs: { team_key: true, match_key: true},
+                async compute(teamMatch) {
+                    return await verifyTeamMatch(teamMatch.team_key, teamMatch.match_key)
+                },
+            }
+        }
+    }
+})
--- src/lib/scripts/TBAVerify.ts
+++ src/lib/scripts/TBAVerify.ts
@@ -1,1 +1,15 @@
-// TOOD: implement data verification with TBA API
+import { TBA_AUTH_KEY } from '$env/static/private'
+
+export async function verifyTeamMatch(team_key: number, match_key: string): Promise<boolean> {
+    const response = await (await fetch(`https://www.thebluealliance.com/api/v3/match/${match_key}/simple`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "X-TBA-Auth-Key": TBA_AUTH_KEY,
+        },
+    })).json()
+
+    const team_keys : string[] = [...response.alliances.blue.team_keys, ...response.alliances.red.team_keys].map((a) => (a.slice(3)))
+
+    return team_keys.includes(team_key.toString())
+}
