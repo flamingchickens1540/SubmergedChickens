365453e43ff00f00b2217573f4ebeb809f8dbe4a
Azalea Colburn <azaleacolburn@gmail.com>
2025-02-23 13:28:54 -0800

    feat(wip)!: finish preload prisma calls (fkey constraint violated on event.create)

--- src/lib/scripts/preload.ts
+++ src/lib/scripts/preload.ts
@@ -1,67 +1,81 @@
 import { prisma } from "$lib/prisma"
 import { API_KEY } from "$env/static/private"
 
+type Match = {
+    match_key: string
+    teams: string[]
+}
+
+// WARNING For some reason prisma doesn't allow object fields to be initialized
+// TeleActions: [] as TeleActionData[],
+// AutoActions: [] as AutoActionData[],
 export async function preload(event_key: string) {
     const match_keys = await getMatchesInEvent(event_key)
     if (!match_keys) return
 
-    const team_matches = (
-        await Promise.all(
-            match_keys.map(async match_key => {
-                return {
-                    match_key,
-                    alliances: await getTeamMatch(match_key),
-                }
-            })
-        )
-    ).filter(({ match_key: _, alliances }) => alliances !== undefined)
+    const matches = await matchKeysToMatches(match_keys)
 
-    const placeholder_team_matches = team_matches.flatMap(
-        ({ match_key, alliances }) =>
-            [...alliances!.red, ...alliances!.blue].map(team_key => {
-                return {
-                    match_key,
-                    team_key: Number.parseInt(team_key.slice(3)),
-                    event_key,
-                    // WARNING For some reason it doesn't allow object fields
-                    // TeleActions: [] as TeleActionData[],
-                    // AutoActions: [] as AutoActionData[],
-                }
-            })
+    const placeholder_team_matches = matches.flatMap(({ match_key, teams }) =>
+        teams.map(team_key => {
+            return {
+                match_key,
+                team_key: parseTBATeam(team_key),
+                event_key,
+            }
+        })
     )
 
-    // Filter out duplicates
-    const team_events = [
-        ...new Set(
-            team_matches.flatMap(team_match => [
-                ...team_match.alliances!.red,
-                ...team_match.alliances!.blue,
-            ])
-        ),
-    ].map(team_key => {
-        return {
-            key: team_key,
-            name: "",
-            // team_event: {
-            //     create:
-            // }
+    // NOTE Using Set to filter out duplicates
+    const team_events = [...new Set(matches.flatMap(match => match.teams))].map(
+        team_key => {
+            return {
+                team_key: parseTBATeam(team_key),
+            }
         }
+    )
+
+    const teams = team_events.map(team_event => {
+        return { key: team_event.team_key, name: "" }
+    })
+
+    await prisma.team.createMany({
+        data: teams,
+        skipDuplicates: true,
     })
 
     await prisma.event.create({
         data: {
             event_key,
             team_events: {
-                createMany: {},
+                createMany: {
+                    data: team_events,
+                },
             },
         },
     })
 
-    await prisma
-
     await prisma.teamMatch.createMany({
         data: placeholder_team_matches,
     })
+}
+
+async function matchKeysToMatches(match_keys: string[]): Promise<Match[]> {
+    const team_matches = (
+        await Promise.all(
+            match_keys.map(async match_key => {
+                return {
+                    match_key,
+                    teams: await getTeamsInMatch(match_key),
+                }
+            })
+        )
+    ).filter(({ match_key: _, teams }) => teams !== undefined) as Match[]
+
+    return team_matches
+}
+
+function parseTBATeam(team_key: string): number {
+    return Number.parseInt(team_key.slice(3))
 }
 
 async function getMatchesInEvent(
@@ -89,9 +103,7 @@ async function getMatchesInEvent(
 /// @param: match_key - `qm1`
 ///
 /// Querie Endpoint: https://www.thebluealliance.com/apidocs/v3
-async function getTeamMatch(
-    match_key: string
-): Promise<{ red: string[]; blue: string[] } | undefined> {
+async function getTeamsInMatch(match_key: string): Promise<string[]> {
     const url = `https://www.thebluealliance.com/api/v3/match/${match_key}`
     const response = await fetch(url, {
         method: "GET",
@@ -103,14 +115,15 @@ async function getTeamMatch(
     })
 
     if (!response.ok) {
-        console.error(`Failed getTeamMatch: ${response.status}`)
-        return
+        console.error(
+            `Failed get teams in match ${match_key}\n\tError ${response.status}`
+        )
+        return []
     }
 
     const alliances = (await response.json())["alliances"]
-
     const red = alliances["red"]["team_keys"]
     const blue = alliances["blue"]["team_keys"]
 
-    return { red, blue }
+    return [...red, ...blue]
 }
