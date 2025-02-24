5f700ece3af595d1b049b006a29ee20492ce1483
Azalea Colburn <azaleacolburn@gmail.com>
2025-02-23 16:20:19 -0800

    fix: a bit of error handling in requesting event keys

--- src/routes/admin/+page.ts
+++ src/routes/admin/+page.ts
@@ -2,15 +2,17 @@ import { error } from "@sveltejs/kit"
 import type { PageLoad } from "./$types"
 
 export const load: PageLoad = async ({ params: _ }) => {
-    const event_keys_res = await fetch("/api/TBAEventKeys", {
+    const res = await fetch("/api/TBAEventKeys", {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
         },
     })
 
-    const tba_event_keys = await event_keys_res.json()
-    console.log(tba_event_keys)
+    const tba_event_keys = res.ok ? await res.json() : []
+    if (tba_event_keys.length === 0) {
+        console.error(`TBA EventKeys request failed: ${res.status}`)
+    }
 
     return { tba_event_keys }
 }
