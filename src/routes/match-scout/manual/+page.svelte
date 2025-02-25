bbf5a1c2302b20263c943856785b3a403d3ffe9d
NateLydem <146988904+belowAverageCoder5188@users.noreply.github.com>
2025-02-24 21:26:51 -0800

    feat: manual scout page

---
+++ src/routes/match-scout/manual/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { goto } from "$app/navigation"
+
+    let team_key = $state("")
+    let match = $state("")
+    let color = $state("")
+</script>
+
+<header
+    class="font-heading grid place-items-center border-b-2 border-white/10 p-2 text-lg font-semibold"
+>
+    <span>Manual Scout</span>
+</header>
+
+<div class="m-2 flex flex-col gap-2">
+    <textarea
+        class="w-full rounded bg-gunmetal p-4"
+        placeholder="Team Number"
+        bind:value={team_key}
+    ></textarea>
+
+    <textarea
+        class="w-full rounded bg-gunmetal p-4"
+        placeholder="Tournament Key"
+        bind:value={match}
+    ></textarea>
+
+    <select
+        name="color"
+        bind:value={color}
+        class="col-span-2 block w-full rounded bg-gunmetal p-4"
+    >
+        <option selected value="">Select a color</option>
+        <option value="blue">Blue</option>
+        <option value="red">Red</option>
+    </select>
+
+    <button
+        class="w-full rounded bg-gunmetal p-4 text-lg font-semibold"
+        onclick={() =>
+            goto(
+                `/match-scout/prematch?match=${match}&team=${team_key}&color=${color}`
+            )}>Scout</button
+    >
+</div>
