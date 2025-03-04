1c5cab52791aa9917be0cdc3c8ac0e10d0f19135
Brandon-Harad <Brandon-Harad@users.noreply.github.com>
2025-03-04 01:12:39 +0000

    style: format files

--- src/routes/pit-scout/image/+page.server.ts
+++ src/routes/pit-scout/image/+page.server.ts
@@ -5,25 +5,24 @@ import { prisma } from "@/prisma"
 
 export const load: PageLoad = async ({ params, url }) => {
     const team_key_string = url.searchParams.get("team")
-    if (!team_key_string) return error(400, "Bad Query Parameters");
+    if (!team_key_string) return error(400, "Bad Query Parameters")
     const team_key = parseInt(team_key_string)
-    
-    const event_key = await getEventKey();
-    if (!event_key) return console.error("No event key set.");
 
-    
+    const event_key = await getEventKey()
+    if (!event_key) return console.error("No event key set.")
+
     const result = await prisma.teamEvent.findUnique({
         where: {
             team_key_event_key: {
                 team_key: team_key,
-                event_key: event_key
-            }
+                event_key: event_key,
+            },
         },
         select: {
-            image: true
-        }
+            image: true,
+        },
     })
 
     if (!result) console.error("Result no gud")
-    return result?.image.map((img) => (img.data))
+    return result?.image.map(img => img.data)
 }
