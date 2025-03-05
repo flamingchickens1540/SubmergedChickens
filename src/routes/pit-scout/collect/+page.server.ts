35eb1ff9e8490d194090cbec88d95559cf29a2ea
Brandon-Harad <Brandon-Harad@users.noreply.github.com>
2025-03-05 23:55:30 +0000

    style: format files

--- src/routes/pit-scout/collect/+page.server.ts
+++ src/routes/pit-scout/collect/+page.server.ts
@@ -1,33 +1,33 @@
 import { error } from "@sveltejs/kit"
 import type { PageLoad } from "./$types"
-import { getEventKey } from "@/scripts/dbUtil";
-import { prisma } from "@/prisma";
+import { getEventKey } from "@/scripts/dbUtil"
+import { prisma } from "@/prisma"
 
 // TODO: Pull information about the event from the database
 export const load: PageLoad = async ({ url }) => {
     const team_key_string = url.searchParams.get("team")
-    if (!team_key_string) return error(400, "Bad Query Parameters");
+    if (!team_key_string) return error(400, "Bad Query Parameters")
     const team_key = parseInt(team_key_string)
-    
-    const event_key = await getEventKey();
-    if (!event_key) return console.error("No event key set.");
+
+    const event_key = await getEventKey()
+    if (!event_key) return console.error("No event key set.")
 
     const result = await prisma.teamEvent.findUnique({
         where: {
             team_key_event_key: {
                 team_key: team_key,
-                event_key: event_key
-            }
+                event_key: event_key,
+            },
         },
         omit: {
             id: true,
             team_key: true,
             imageCollected: true,
-        }
+        },
     })
 
-    if (!result) console.error("Result no gud");
+    if (!result) console.error("Result no gud")
 
-    if (!result?.dataCollected) return null;
+    if (!result?.dataCollected) return null
     return result
 }
