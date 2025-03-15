bea567891b390783dcd9eab132f44744ddaed97b
autumnwilkes <autumnawilkes@gmail.com>
2025-03-15 10:32:03 -0700

    fix: endgame type

--- src/lib/scripts/TBAUpdateTM.ts
+++ src/lib/scripts/TBAUpdateTM.ts
@@ -1,6 +1,7 @@
 import { API_KEY } from "$env/static/private"
 import { getEventKey } from "./dbUtil"
 import { warn } from "@/consoleUtils"
+import { Endgame } from "@prisma/client"
 import { prisma } from "$lib/prisma"
 
 export async function updateTBAMatchData(match_key: string) {
@@ -32,31 +33,78 @@ export async function updateTBAMatchData(match_key: string) {
         match["alliances"]["blue"]["team_keys"] as string[]
     ).map(team => Number.parseInt(team.slice(3)))
     const scores = match["score_breakdown"]
+    const recorded_endgames = await prisma.teamMatch.findMany({
+        where: {
+            match_key: {
+                equals: verbose_match_key,
+            },
+        },
+        select: {
+            endgame: true,
+            team_key: true,
+        },
+    })
+
     for (let i = 1; i <= 3; i++) {
-        console.log(`${verbose_match_key}, ${red[i - 1]} `)
+        const red_endgame = determineEndgame(
+            scores["red"][`endgameRobot${i}`],
+            recorded_endgames.find(match => match.team_key === red[i - 1])
+                ?.endgame ?? Endgame.None
+        )
+
+        const blue_endgame = determineEndgame(
+            scores["blue"][`endgameRobot${i}`],
+            recorded_endgames.find(match => match.team_key === blue[i - 1])
+                ?.endgame ?? Endgame.None
+        )
+
         prisma.teamMatch.update({
             where: {
                 id_key: {
-                    match_key: match_key,
+                    match_key: verbose_match_key,
                     team_key: red[i - 1],
                 },
             },
             data: {
-                auto_leave_start: true, // scores["red"][`autoLineRobot${i}`] === "Yes",
-                // endgame: scores["red"][`endGameRobot${i}`].toString(), // None, Parked, ShallowCage, DeepCage
+                auto_leave_start: scores["red"][`autoLineRobot${i}`] === "Yes",
+                endgame: red_endgame, // None, Parked, ShallowCage, DeepCage
             },
         })
         prisma.teamMatch.update({
             where: {
                 id_key: {
-                    match_key: match_key,
+                    match_key: verbose_match_key,
                     team_key: blue[i - 1],
                 },
             },
             data: {
                 auto_leave_start: true, // scores["blue"][`autoLineRobot${i}`] == "Yes",
-                // endgame: scores["blue"][`endGameRobot${i}`],
+                endgame: blue_endgame,
             },
         })
     }
+}
+
+function determineEndgame(
+    tba_endgame: string,
+    scout_endgame: Endgame
+): Endgame {
+    if (tba_endgame === "ShallowCage") {
+        return Endgame.Shallow
+    }
+    if (tba_endgame === "DeepCage") {
+        return Endgame.Deep
+    }
+    // If the scout records a robot as climbed, and that robot didn't climb, then it presumably failed to climb
+    // we could therefor remove the "failed" option (unless we want it simply for the sake of the scouts) and tell scouts to always record fails as successes
+    switch (scout_endgame) {
+        case Endgame.Fail:
+        case Endgame.Deep:
+        case Endgame.Shallow:
+            return Endgame.Fail
+    }
+    if (tba_endgame === "Parked") {
+        return Endgame.Park
+    }
+    return Endgame.None
 }
