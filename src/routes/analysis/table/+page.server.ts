75546c561b2f52b25b2dacffb762b0cc88b2e4d5
Brandon Harad <haradb@catlin.edu>
2025-03-19 16:44:13 -0700

    feat: ability

--- src/routes/analysis/table/+page.server.ts
+++ src/routes/analysis/table/+page.server.ts
@@ -1,27 +1,19 @@
 import { error } from "@/consoleUtils"
 import type { RequestHandler } from "../$types"
 import { prisma } from "@/prisma"
-import type { Team, TeamMatch } from "@prisma/client"
+import type { Team, TeamEvent, TeamMatch } from "@prisma/client"
 import { json } from "@sveltejs/kit"
 import Action from "../../match-scout/Action.svelte"
+import { getEventKey } from "@/scripts/dbUtil"
 
 export const POST: RequestHandler = async ({
     request,
 }: any): Promise<Response> => {
     const team_key: number = await request.json()
 
-    let team : Team | null = await prisma.team.findUnique({
-        where: {
-            key: team_key
-        }
-    })
-
-    if (!team) error("team_key is not valid");
-    team = team as Team
-
     let matches = await prisma.teamMatch.findMany({
         where: {
-            team: team
+            team_key: team_key
         }
     })
 
@@ -38,7 +30,25 @@ export const POST: RequestHandler = async ({
         algae_results.set(number, algaeScored(match))
     }
 
+    const event_key = await getEventKey() ?? (function () {console.error("No event key found"); return ""})()
+    let results = await prisma.teamEvent.findUnique({
+        where: {
+            team_key_event_key: {
+                team_key: team_key,
+                event_key: event_key
+            }
+        }
+    })
+
+    if(!results) console.error("No team event found");
+    results = results as TeamEvent
+
     let ability: string = ""
+    ability += `Coral:${coralScoreLevels(results)};`
+    ability += `Algae:${algaeScoreLocations(results)}`
+    ability += `Clean:${cleanScoreLevels(results)};`
+    ability += `Climb:${climbAbility(results)}`
+
     return json({
         coral_results,
         algae_results,
@@ -68,4 +78,34 @@ function algaeScored(match: TeamMatch): number {
         match.tele_score_net_succeed ?? 0,
         match.tele_score_processor_succeed ?? 0,
     ].reduce((a,b) => (a+b))
+}
+
+function coralScoreLevels(results: TeamEvent) {
+    let toBeReturned = "";
+    if (results.coralScoreL1) toBeReturned += "1"
+    if (results.coralScoreL2) toBeReturned += "2"
+    if (results.coralScoreL3) toBeReturned += "3"
+    if (results.coralScoreL4) toBeReturned += "4"
+    return toBeReturned
+}
+
+function cleanScoreLevels(results: TeamEvent) {
+    let toBeReturned = "";
+    if (results.cleanScoreL2) toBeReturned += "2"
+    if (results.cleanScoreL3) toBeReturned += "3"
+    return toBeReturned
+}
+
+function algaeScoreLocations(results: TeamEvent) {
+    let toBeReturned = "";
+    if (results.algaeScoreProcessor) toBeReturned += "P"
+    if (results.algaeScoreNet) toBeReturned += "N"
+    return toBeReturned
+}
+
+function climbAbility(results: TeamEvent) {
+    let toBeReturned = "";
+    if (results.shallowClimb) toBeReturned += "S"
+    if (results.deepClimb) toBeReturned += "D"
+    return toBeReturned
 }
