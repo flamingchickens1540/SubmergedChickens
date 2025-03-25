57b230d8ffbee8695e67ab9ad3da33c414fb8beb
autumnwilkes <autumnawilkes@gmail.com>
2025-03-24 18:27:46 -0700

    fix(wip): re-adding correct representation of submitted matches, some ui rearrangement

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -21,16 +21,15 @@
         tm_status: "Queue" | "Pending" | "Submitted" | "Removed"
     }
 
-    type PendingTeamMatch = {
+    type PendingSubmission = {
         team_key: string
-        color: string
-        scout: string
-        displaying_tk: boolean
+        scout?: string
     }
 
+    // honestly I really don't care about the ability to see who scouted previous robots, I don't find that section of the menu relevant as a whole beyond the potentail ability to view user's submitted data
     type SubmittedTeamMatch = UncountedTeamMatch & {
         scout_username?: string
-        displaying_tmk: boolean
+        displaying_tk: boolean
     }
 
     let { data }: PageProps = $props()
@@ -48,8 +47,12 @@
     let current_robots: LocalStore<CurrentRobot[]> = $state(
         localStore("current_robots", [])
     )
+    let pending_submissions: LocalStore<PendingSubmission[]> = localStore(
+        "pending_submissions",
+        []
+    )
     let scout_queue: string[] = $state([])
-    let robot_queue: QueuedTeamMatch[] = $state([])
+    let robot_queue: QueuedTeamMatch[] = []
 
     let submitted_team_matches: LocalStore<SubmittedTeamMatch[]> = $state(
         localStore("submitted_team_matches", [])
@@ -95,16 +98,16 @@
         if (index === -1) return
 
         robot_queue.splice(index, 1)
-        const sent_robot = current_robots.value.find((robot: CurrentRobot) => {
-            robot.team_key === team_data.team_key
-        })
+        const sent_robot = current_robots.value.find(
+            (robot: CurrentRobot) => robot.team_key === team_data.team_key
+        )
         if (!sent_robot) return
         sent_robot!.tm_status = "Pending"
         sent_robot.scout = scout
     })
 
     socket.on("new_team_match", (team_match: UncountedTeamMatch) => {
-        const submitted: CurrentRobot = current_robots.value.find(
+        const submitted: CurrentRobot | undefined = current_robots.value.find(
             (robot: CurrentRobot) =>
                 parseInt(robot.team_key) === team_match.team_key
         ) ?? {
@@ -118,9 +121,8 @@
 
         const submitted_team_match = {
             ...team_match,
-            color: submitted.color,
             scout_username: submitted.scout,
-            displaying_tmk: true,
+            displaying_tk: true,
         }
 
         submitted_team_matches.value.push(submitted_team_match)
@@ -154,6 +156,9 @@
                 return { team_key, color: "blue" }
             }),
         ]
+        pending_submissions.value = current_robots.value
+            .filter(tm => tm.tm_status === "Pending")
+            .map(tm => tm as PendingSubmission)
         current_robots.value = robot_queue.map(({ team_key, color }) => {
             return {
                 team_key,
@@ -163,9 +168,6 @@
             }
         })
 
-        next_match_key.value =
-            next_match_key.value.slice(0, 2) +
-            (Number.parseInt(next_match_key.value.slice(2)) + 1).toString()
         next_red_robots.value = ["", "", ""]
         next_blue_robots.value = ["", "", ""]
     }
@@ -177,6 +179,12 @@
         scout_queue.splice(index, 1)
 
         socket.emit("leave_scout_queue", scout_id)
+    }
+    const clear_scout_queue = () => {
+        for (const scout of scout_queue) {
+            socket.emit("leave_scout_queue", scout)
+        }
+        scout_queue = []
     }
 
     const auto_load_teams = async () => {
@@ -249,9 +257,9 @@
 </script>
 
 <div
-    class="m-auto grid max-w-6xl grid-cols-2 grid-rows-5 gap-2 p-2 sm:grid-cols-3 sm:grid-rows-2 sm:gap-4 md:grid-cols-5"
+    class="m-auto grid max-w-6xl grid-cols-4 grid-rows-5 gap-2 p-2 sm:grid-cols-3 sm:grid-rows-2 sm:gap-4 md:grid-cols-5"
 >
-    <div class="col-span-2 row-span-2 grid grid-cols-subgrid grid-rows-subgrid">
+    <div class="col-span-2 row-span-5 grid grid-cols-subgrid grid-rows-subgrid">
         <div class="col-span-2 grid grid-cols-3 gap-2 rounded bg-gunmetal p-2">
             <input
                 bind:value={next_match_key.value}
@@ -282,7 +290,7 @@
         </div>
         <div class="col-span-2 flex flex-col gap-2 rounded bg-gunmetal p-2">
             <span class="col-span-3 text-center">Current Robots</span>
-            <div class="grid max-h-28 grid-cols-3 gap-2 overflow-y-scroll">
+            <div class="grid max-h-28 grid-cols-3 gap-2">
                 {#each current_robots.value as { team_key, color, scout, displaying_tk, tm_status }, i}
                     <button
                         class="grid h-12 grid-cols-2 place-items-center rounded {tm_status ==
@@ -306,20 +314,24 @@
                             {:else}
                                 {scout}
                             {/if}
+                            <div
+                                class="size-6 rounded-full bg-{color === 'red'
+                                    ? 'bittersweet'
+                                    : 'steel_blue'}"
+                            ></div>
                         {/if}
-
-                        <div
-                            class="size-6 rounded-full bg-{color === 'red'
-                                ? 'bittersweet'
-                                : 'steel_blue'}"
-                        ></div>
                     </button>
                 {/each}
             </div>
         </div>
     </div>
-    <div class="row-span-2 flex flex-col gap-2 rounded bg-gunmetal p-2">
-        <span class="text-center">Scout Queue</span>
+    <div
+        class="col-span-1 row-span-2 flex flex-col gap-2 rounded bg-gunmetal p-2"
+    >
+        <div class="grid grid-cols-3">
+            <span class="col-span-2 text-center">Scout Queue</span>
+            <button class=" rounded bg-eerie_black">Clear</button>
+        </div>
         <div class="grid max-h-64 grid-cols-1 gap-2 overflow-y-scroll">
             {#each scout_queue as scout}
                 <div
@@ -335,16 +347,19 @@
         </div>
     </div>
     <div class="row-span-2 flex flex-col gap-2 rounded bg-gunmetal p-2">
-        <span class="text-center">Past Team Matches</span>
+        <div class="grid grid-cols-3">
+            <span class="col-span-2 text-center">Scout Queue</span>
+            <button class=" rounded bg-eerie_black">Clear</button>
+        </div>
         <div class="grid max-h-64 grid-cols-1 gap-2 overflow-y-scroll">
             {#each submitted_team_matches.value as team_match, i}
                 <button
                     class="grid h-12 grid-cols-1 place-items-center rounded bg-jungle_green p-2 text-center"
                     onclick={() =>
-                        (submitted_team_matches.value[i].displaying_tmk =
-                            !team_match.displaying_tmk)}
+                        (submitted_team_matches.value[i].displaying_tk =
+                            !team_match.displaying_tk)}
                 >
-                    {#if team_match.displaying_tmk}
+                    {#if team_match.displaying_tk}
                         {team_match.match_key} {team_match.team_key}
                     {:else}
                         {team_match.scout_username}
